d64ded65425298b7d0d52d4b3b748203fd6d73f3
victoriarubiolopez <victoriarubio.18@gmail.com>
2024-09-21 10:48:26 -0600

    changes 1

--- cypress/e2e/Sc2_Test..cy.js
+++ cypress/e2e/Sc2_Test..cy.js
@@ -2,9 +2,15 @@ import { smartdata } from "./AutoTest/Scenario2/Data_Elements_Methods/smart.data
 
 describe('Scenario Smart Tv', () => {
     it ('passes', () => {
-        cy.visit(smartdata.url) 
-        cy.get('#filter-p_36 .a-section').contains('Rrecio')
-    }
+        cy.visit(playdata.url)
+        cy.get('#twotabsearchtextbox').type('smart tv')
+        cy.get(".nav-input#nav-search-submit-button").click()
+        cy.get('#filter-p_36 .a-size-base').contains('Precio')
+        cy.get('#p_n_size_browse-bin-title .a-size-base').contains('Marca')
+        cy.get('.a-spacing-micro div .a-label').contains('Sony').click()
+        cy.get('#p_n_size_browse-bin-title .a-size-base').contains('Tamaño de Pantalla')
+        cy.get('.a-spacing-micro div .a-label').contains('48 a 55 in').click()
+            }
 )
 }
 )
