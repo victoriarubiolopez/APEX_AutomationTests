e5235314ef3fffc9d37da3637eb323de340bd25d
victoriarubiolopez <victoriarubio.18@gmail.com>
2024-09-20 21:37:26 -0600

    changes playdata

--- cypress/e2e/Sc1_Test..cy.js
+++ cypress/e2e/Sc1_Test..cy.js
@@ -11,7 +11,7 @@ describe ('Search for Playstation 5', () =>{
         cy.get('.a-section div .a-link-normal .a-size-base').contains('Consolas de PlayStation 4')
         cy.get('.a-section div .a-link-normal .a-size-base').contains('Juegos de PlayStation 5')
         // select an item
-        Playmethods.ClickonItemeSelection(PLaydata.SelectedItem)
+        Playmethods.ClickonItemeSelection(playdata.SelectedItem)
         cy.get('#titleSection').contains('PlayStation®5 (Modelo Slim) - Pack con 2 Juegos - Digital').should('be.visible')
         //valida price
         cy.get('#corePrice_feature_div').contains('$9,189.00')
