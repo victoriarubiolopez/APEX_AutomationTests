839868e216300b53dbba8e5d24e7f70ffdddeddb
victoriarubiolopez <victoriarubio.18@gmail.com>
2024-09-21 01:31:05 -0600

    Creacion Scenario 2 Smart Tv 1

---
+++ cypress/e2e/AutoTest/Scenario2/Data_Elements_Methods/smart.data.js
@@ -0,0 +1,10 @@
+export class smartdata {
+    static get keysearchword () {
+        return 'smart tv';
+
+    }
+    static get url () {
+        return 'www.amazon.com.mx';
+
+    }
+}
---
+++ cypress/e2e/AutoTest/Scenario2/Data_Elements_Methods/smart.elements.js
@@ -0,0 +1,12 @@
+export class smartelements {
+    /*static get filters(){
+        return {
+            static get Price() {
+                cy.get('div[class="a-section]').
+                find('[id^="filter-p_36"]')
+                .eq(0)
+
+        }
+    }
+}*/
+}
