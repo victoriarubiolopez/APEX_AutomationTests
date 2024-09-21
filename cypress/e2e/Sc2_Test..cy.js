0e723d53771d2c3fc0166cf5759e286ab9b82282
victoriarubiolopez <victoriarubio.18@gmail.com>
2024-09-21 01:41:24 -0600

    changes sc2 2

--- cypress/e2e/Sc2_Test..cy.js
+++ cypress/e2e/Sc2_Test..cy.js
@@ -3,7 +3,7 @@ import { smartdata } from "./AutoTest/Scenario2/Data_Elements_Methods/smart.data
 describe('Scenario Smart Tv', () => {
     it ('passes', () => {
         cy.visit(smartdata.url) 
-        cy.get('div[class="a-section]').
+        cy.get('div[class="a-section"]').
                 find('[id^="filter-p_36"]')
                 .eq(0)
     }
