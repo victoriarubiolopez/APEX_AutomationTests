df26d2c72225ff8c546757ef64af773c96c7c96e
victoriarubiolopez <victoriarubio.18@gmail.com>
2024-09-21 12:43:10 -0600

    clean code 1

--- cypress/e2e/AutoTest/Scenario1/Data_Elements_Methods/playstation.elements.js
+++ cypress/e2e/AutoTest/Scenario1/Data_Elements_Methods/playstation.elements.js
@@ -9,15 +9,11 @@ export class PlayElements {
     }
     static get labels(){
         return{
-            get ResultsTitle(){
-                return cy.get('.a-section div .s-no-outline .a-size-medium-plus ').contains('Resultados')
-                },
+            
             get SelectedItem(){
                 return     cy.contains('PlayStation®5 (Modelo Slim) - Pack con 2 Juegos - Digital')
             },
-            get Price(){
-                return     cy.get("$9,189.00")
-            }
+            
         }
     }
 
