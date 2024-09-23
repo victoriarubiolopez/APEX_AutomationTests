21093c336b7446305aa24c14d8d28fb548fdb04d
victoriarubiolopez <victoriarubio.18@gmail.com>
2024-09-22 18:18:37 -0600

    Sc2

--- cypress/e2e/Sc2_Test..cy.js
+++ cypress/e2e/Sc2_Test..cy.js
@@ -1,6 +1,20 @@
+import { commondata } from "./AutoTest/CommonPage/common.data"
+import { CommonElements } from "./AutoTest/CommonPage/common.elements"
+import { CommonMethods } from "./AutoTest/CommonPage/common.methods"
+import { HomeData } from "./AutoTest/PageHome/Home.data"
+import { PageResultsElements } from "./AutoTest/PageResults/PageResults.Elements"
+import { PageResultsMethods } from "./AutoTest/PageResults/PageResults.Methods"
+
 describe('Scenario Smart TV', ()=> {
     it('passes', ()=>{
-
+        cy.visit(commondata.url)
+        CommonMethods.InsertSearchWord(HomeData.BuscaSmatTV)
+        CommonMethods.ClickOnSearchButton()
+        //PageResults)
+        cy.get(PageResultsElements.Filters.Precio)
+        cy.get(PageResultsElements.Filters.Tamaño)
+        cy.get(PageResultsElements.Filters.Marca)
+        
     }
     )
 }
