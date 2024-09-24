a86ce99a5b1783fd03df9e43b7ae309eb066e72e
victoriarubiolopez <victoriarubio.18@gmail.com>
2024-09-24 10:58:30 -0600

    Training_Filters

--- cypress/e2e/AutoTest/Training.js
+++ cypress/e2e/AutoTest/Training.js
@@ -7,11 +7,12 @@ describe ('PruebayError', () =>{
         cy.visit(commondata.url)     
         CommonMethods.InsertSearchWord(HomeData.BuscaPlaystation)
         CommonMethods.ClickOnSearchButton()
-        cy.wait(10000);
+        cy.get('[type="checkbox"]').check('Sony')
+        /* cy.wait(10000);
         cy.get()
         cy.get('#brandsRefinements').find('.a-unordered-list .a-link-normal .a-size-base').eq(1).should('be-checked')
         cy.contains('48 a 55 in ').should('be-checked')
-        cy.get('#search div .a-section').contains('resultados para')
+        cy.get('#search div .a-section').contains('resultados para') */
 
 
     }
